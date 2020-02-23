49620882daf632d329563f6fb505c63c8ecd849e
Alexandr Moroz <afmoroz@gmail.com>
2020-02-24 01:02:46 +0200

    removed some files

--- src/pages/WithoutTransition/index.tsx
+++ src/pages/WithoutTransition/index.tsx
@@ -1,8 +1,17 @@
 import React from "react";
-import Test from "./Test/Test";
+import { Provider } from "./Context/context";
+import DragWrapper from "./DragWrapper/DragWrapper";
+import Card from "./Card/Card";
 
 const WithoutTransition = () => {
-  return <Test />;
+  return (
+    <Provider>
+      <DragWrapper>
+        <Card>
+        </Card>
+      </DragWrapper>
+    </Provider>
+  );
 };
 
 export default WithoutTransition;
